94f1da6af7e28105f8a084face5db355f514bd57
farahsamsi <1827034farahsamsiprome@gmail.com>
2025-05-15 20:34:18 +0600

    added increase and decrease medicine quantity functionalities for cart page from backend

--- index.js
+++ index.js
@@ -5,13 +5,13 @@ require("dotenv").config();
 
 const port = process.env.PORT || 5000;
 
+// mongoDB connection
+const { MongoClient, ServerApiVersion, ObjectId } = require("mongodb");
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.tu4i6.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0`;
+
 // middleware
 app.use(cors());
 app.use(express.json());
-
-// mongoDB connection
-const { MongoClient, ServerApiVersion } = require("mongodb");
-const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.tu4i6.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0`;
 
 // Create a MongoClient with a MongoClientOptions object to set the Stable API version
 const client = new MongoClient(uri, {
@@ -57,14 +57,64 @@ async function run() {
     // ---------  Carts collection API
     app.get("/carts", async (req, res) => {
       const email = req.query.email;
-      const query = { email: email };
+      const query = { buyerEmail: email };
       const result = await cartCollection.find(query).toArray();
       res.send(result);
     });
 
     app.post("/carts", async (req, res) => {
       const cartItem = req.body;
+      const { buyerEmail, medicineName } = cartItem;
+
+      const isExisting = await cartCollection.findOne({
+        buyerEmail,
+        medicineName,
+      });
+      if (isExisting) {
+        return res
+          .status(400)
+          .send({ message: "This medicine is already in the cart." });
+      }
+
       const result = await cartCollection.insertOne(cartItem);
+      res.send(result);
+    });
+
+    // PATCH: Update quantity and total price
+    app.patch("/carts/:id", async (req, res) => {
+      const id = req.params.id;
+      const { type } = req.body; // "increase" or "decrease"
+      const query = { _id: id };
+
+      const cartItem = await cartCollection.findOne(query);
+
+      let newQuantity = cartItem?.medicineQuantity;
+      if (type === "increase") {
+        newQuantity += 1;
+      } else if (type === "decrease" && cartItem?.medicineQuantity > 1) {
+        newQuantity -= 1;
+      } else {
+        return res.status(400).send({ message: "Invalid quantity update" });
+      }
+
+      const newTotal = parseInt(newQuantity * cartItem?.price);
+
+      const updatedDoc = {
+        $set: {
+          medicineQuantity: newQuantity,
+          totalPrice: newTotal,
+        },
+      };
+
+      const result = await cartCollection.updateOne(query, updatedDoc);
+
+      res.send(result);
+    });
+
+    app.get("/carts/:id", async (req, res) => {
+      const id = req.params.id;
+      const query = { _id: id };
+      const result = await cartCollection.findOne(query);
       res.send(result);
     });
 
